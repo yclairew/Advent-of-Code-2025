09246c24be591258734d0ea9aaca6a4e4433ddd1
yclairew <y.clairewu@gmail.com>
2025-12-03 02:25:28 -0600

    Convert day1 from submodule to normal folder

---
+++ day1/day1-part1.py
@@ -0,0 +1,28 @@
+with open("input.txt", "r") as file:
+    content = file.read()
+    steps = content.split("\n")
+
+current = 50 # starts at 50
+times = 0
+
+for step in steps:
+    print(f"Step: {step}")
+    if step[0] == 'R':
+        current += int(step[1:])
+        # if > 99
+        if current > 99:
+            current = current % 100
+    elif step[0] == 'L':
+        current -= int(step[1:])
+        # if negative
+        if current < 0:
+            current = (current + 100) % 100;
+    else:
+        continue
+    
+    # check if ends at 0
+    if current == 0:
+        times += 1
+        print(times)
+
+print(times)
